7fae4136934b7d6e9808e85bf4eb95657261e650
roymandev <royman.dev@gmail.com>
2023-11-25 12:37:31 +0700

    refactor: CardAnime

--- src/components/Card/CardAnime.tsx
+++ src/components/Card/CardAnime.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, ViewStyle } from 'react-native';
-import { Card } from 'react-native-paper';
+import { StyleSheet, View, ViewStyle } from 'react-native';
+import { Text } from 'react-native-paper';
 
 import { Image } from 'expo-image';
 
@@ -14,33 +14,32 @@ type Props = {
 
 function CardAnime({ anime, sx }: Props) {
   return (
-    <Card style={[styles.container, sx?.container]} mode="contained">
+    <View style={[styles.container, sx?.container]}>
       {anime.main_picture?.medium && (
         <Image
           source={{ uri: anime.main_picture.medium }}
           style={styles.image}
         />
       )}
-      <Card.Title
-        title={anime.title}
-        titleNumberOfLines={2}
-        titleStyle={{ lineHeight: 18, paddingLeft: 0 }}
-        style={{
-          paddingLeft: 0,
-          paddingRight: 0,
-        }}
-        titleVariant="titleSmall"
-      />
-    </Card>
+      <Text
+        variant="titleSmall"
+        numberOfLines={2}
+        style={{ lineHeight: 18, paddingLeft: 0, height: 36 }}
+      >
+        {anime.title}
+      </Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'transparent',
+    gap: 8,
+    height: 240,
   },
   image: {
-    height: 195,
+    flex: 1,
     borderRadius: 16,
   },
 });
